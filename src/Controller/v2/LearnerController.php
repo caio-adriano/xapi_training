00f51b435745fa15f3dcc8c1545f8639140696c3
caio de freitas adriano <caio.defreitas@globant.com>
2021-03-11 16:46:52 -0300

    Created endpoint to get one learner

--- src/Controller/v2/LearnerController.php
+++ src/Controller/v2/LearnerController.php
@@ -5,6 +5,7 @@ namespace App\Controller\v2;
 use App\Entity\Learner;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Rest\Route("v2/learners")
@@ -21,6 +22,19 @@ class LearnerController extends AbstractFOSRestController
         $learners = $learnerRespositoy->findAll();
 
         $view = $this->view($learners);
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * @Rest\Get("/{id}", name="get_one_learner")
+     */
+    public function show($id): Response
+    {
+        $learnerRespository = $this->getDoctrine()->getRepository(Learner::class);
+
+        $learner = $learnerRespository->find($id);
+        $view    = $this->view($learner);
 
         return $this->handleView($view);
     }
