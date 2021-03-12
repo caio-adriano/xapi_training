1c1a1322f9fb30ceec858646326e076aeccf3073
caio de freitas adriano <caio.defreitas@globant.com>
2021-03-12 10:26:51 -0300

    created endpoint (v2) to generate a new manager

--- src/Controller/v2/ManagerController.php
+++ src/Controller/v2/ManagerController.php
@@ -5,6 +5,10 @@ namespace App\Controller\v2;
 use App\Entity\Manager;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Exception\InvalidParameterException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @Rest\Route("v2/managers")
@@ -21,6 +25,34 @@ class ManagerController extends AbstractFOSRestController
         $managers = $managerRepository->findAll();
 
         $view = $this->view($managers);
+        return $this->handleView($view);
+    }
+
+    /**
+     * @Rest\Post("", name="create a manager")
+     */
+    public function create(Request $request, ValidatorInterface $validator): Response
+    {
+        $em      = $this->getDoctrine()->getManager();
+        $dateNow = new \DateTime('now', new \DateTimeZone('America/Sao_Paulo'));
+
+        $requestData              = json_decode($request->getContent(), true);
+        $requestData['createdAt'] = $dateNow;
+        $requestData['updatedAt'] = $dateNow;
+
+        $manager = new Manager();
+        $manager->load($requestData);
+
+        $errors = $validator->validate($manager);
+        if (0 !== count($errors)) {
+            throw new InvalidParameterException((string) $errors);
+        }
+
+        $em->persist($manager);
+        $em->flush();
+
+        $view = $this->view($manager);
+
         return $this->handleView($view);
     }
 }
