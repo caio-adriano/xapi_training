b00eecc35a0e1a99f074a6733e3ce5fd0250b259
caio de freitas adriano <caio.defreitas@globant.com>
2021-03-17 11:45:57 -0300

    Fix cache for unit test

--- src/Lib/Test/AbstractApiTestCase.php
+++ src/Lib/Test/AbstractApiTestCase.php
@@ -4,6 +4,7 @@ namespace App\Lib\Test;
 
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
@@ -17,8 +18,14 @@ abstract class AbstractApiTestCase extends WebTestCase
     /** @var Serializer */
     protected $serializer;
 
+    /** @var RedisAdapter */
+    protected $cache;
+
     public function setUp(): void
     {
+        $this->cache = new RedisAdapter(RedisAdapter::createConnection($_SERVER['REDIS_PROTOCOL'] . $_SERVER['REDIS_HOST']));
+        $this->cache->clear();
+
         $this->client = static::createClient();
         $this->serializer = new Serializer([
             new ObjectNormalizer(),
